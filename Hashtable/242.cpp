606158c507371b34638a6bfb4c22f736a8ce0861
Amogh Joshi <amoghj8@gmail.com>
2020-08-19 16:23:49 +0530

    242. Valid Anagram

---
+++ Hashtable/242.cpp
@@ -0,0 +1,22 @@
+// 242. Valid Anagram
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+            return false;
+        unordered_map<char,int> m;
+        for(char ch: s) {
+            m[ch]++;
+        }
+        for(char ch:t) {
+            if(m.find(ch)==m.end() || m[ch]==0) {
+                return false;
+            }
+            else {
+                m[ch]--;
+            }
+        }
+        return true;
+    }
+};
