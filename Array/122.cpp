2b0b19ce4e2a47c8545bcf478a70dee52f250f39
Amogh Joshi <amoghj8@gmail.com>
2020-08-21 13:18:04 +0530

    122. Best Time to Buy and Sell Stock II

---
+++ Array/122.cpp
@@ -0,0 +1,16 @@
+// 122. Best Time to Buy and Sell Stock II
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()==1)
+            return 0;
+        
+        int profit = 0;
+        
+        for(int i=1;i<prices.size();i++) {
+            profit+=max(prices[i]-prices[i-1], 0);
+        }
+        return profit;
+    }
+};
