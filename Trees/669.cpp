e3793fd30393f06c4bdd1317cbbda368a642a383
Amogh Joshi <amoghj8@gmail.com>
2020-08-22 14:39:23 +0530

    669. Trim a Binary Search Tree

---
+++ Trees/669.cpp
@@ -0,0 +1,32 @@
+//669. Trim a Binary Search Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if(!root)
+            return nullptr;
+        
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        
+        if(root->val>=L && root->val<=R)
+            return root;
+        else if(root->val<L)
+            return trimBST(root->right, L, R);
+        else if(root->val>R)
+            return trimBST(root->left, L, R);
+        
+        return root;    
+    }
+};
